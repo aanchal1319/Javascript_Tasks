1d7ca86c14961610330c73ba13b84af423e3bbeb
aanchal patel <aanchalpatel357@gmail.com>
2025-09-04 10:58:21 +0530

    Add task1

---
+++ task1/task1.js
@@ -0,0 +1,3 @@
+/*.  Create a js file which contains variable declaration for a form which asks following basic details :
+(What is your Name, Email, age, address, phone number, gender,Are you married) */
+let fname,email,age,PhoneNo,address,married,gender;
